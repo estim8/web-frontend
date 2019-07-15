36cf037e454a35f0a3bf82c69283dc744de0a361
Lars Baunwall <larslb@thinkability.dk>
2019-07-15 21:57:10 +0200

    Add routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+
+import StartGame from "@/components/games/StartGame.vue";
+import GameSession from "@/components/games/GameSession.vue"
 
 Vue.use(Router);
 
@@ -9,17 +11,22 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '/play',
+      name: 'play',
+      component: StartGame,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      path: "/play/:gameId",
+      name: "play-session",
+      component: GameSession,
     },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    // },
   ],
 });
